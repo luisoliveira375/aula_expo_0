ad3a33a065145ddb25cfa26a21dbee5b349302a0
luisoliveira375 <160646979+luisoliveira375@users.noreply.github.com>
2024-02-27 15:24:09 -0300

    aula 27-02

---
+++ componente/exemplo_0/index.js
@@ -0,0 +1,13 @@
+import {view, text } from 'reac-native';
+
+function exemplo 0 () {
+    return(
+        <view>
+            <text>minha primeira  tela</text>
+        </view>
+
+    );
+
+}
+
+export default exemplo 0;
