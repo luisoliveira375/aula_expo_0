2d27b635af28aa631cf0e1866a1d184726b8dcc9
luisoliveira375 <160646979+luisoliveira375@users.noreply.github.com>
2024-03-01 09:48:26 -0300

    aula 01-03

---
+++ components/exemplo_0/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+
+function exemplo_0() {
+ return(
+    <viem>
+        <text style={{color: '#fff'}}> Minha Primeira Tela</text>
+    </viem>
+ );
+
+}
+
+export default exemplo_0;
