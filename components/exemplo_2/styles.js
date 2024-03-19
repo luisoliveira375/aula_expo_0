f7504abf455304a9db28516404520a24071c3bf6
luisoliveira375 <160646979+luisoliveira375@users.noreply.github.com>
2024-03-19 15:23:40 -0300

    aula 19-03

---
+++ components/exemplo_2/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+import Exemplo2 from '.';
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#fff',
+    },
+})
+
+export default styles;
