c01c8053a3f4925ee6cdec8488b9b0279d1357ef
luisoliveira375 <160646979+luisoliveira375@users.noreply.github.com>
2024-04-30 15:25:23 -0300

    30-04

---
+++ components/exemplo_3/index.js
@@ -0,0 +1,14 @@
+import {View, Text, Pressable} from 'react-native';
+
+import styles from './styles'
+
+export default function Exemplo_3 () {
+    return(
+        <View>
+            <Text>Exemplo 3</Text>
+            <Pressable>
+                <Text>Ex. botão</Text>
+            </Pressable>
+        </View>
+    );
+}
---
+++ components/exemplo_3/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#d6eeff',
+        borderRadius: 20,
+    },
+    titulo: {
+        fontSize: 30,
+        fontWeight: 'bold', 
+        marginVertical: 20,
+        color: '#673AB7'
+    },
+
+});
+
+export default styles;
+
+
+
