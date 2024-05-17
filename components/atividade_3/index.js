7093ce37e894f39ef85cde7855756a6c47c6830d
luisoliveira375 <160646979+luisoliveira375@users.noreply.github.com>
2024-05-17 09:46:40 -0300

    17-05

---
+++ components/atividade_3/index.js
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import { View, Text, Pressable } from 'react-native';
+
+import styles from './styles';
+
+function atividade_3() {
+
+    const [numero, setNumero] = useState(0);
+
+
+    function incremento () {
+        setNumero(numero + 1);
+    }
+
+    function menos () {
+        setNumero(numero - 1);
+    }
+
+    function zero () {
+        setNumero(numero - numero);
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.titulo}>atividade_3</Text>
+          
+            <Text style={styles.txtNumero}>{numero}</Text>
+
+            <Pressable
+                onPress={() => incremento ()}
+                style={({ pressed }) => pressed ?
+                [styles.botao, styles.botaoPress]
+                :
+                styles.botao}
+            >
+                <Text style={styles.txtBotao}>+1</Text>
+            </Pressable>
+
+            <Pressable
+                onPress={() => menos ()}
+                style={({ pressed }) => pressed ?
+                [styles.botao, styles.botaoPress]
+                :
+                styles.botao}
+            >
+                <Text style={styles.txtBotao}>-1</Text>
+            </Pressable>
+
+            <Pressable
+                onPress={() => zero ()}
+                style={({ pressed }) => pressed ?
+                [styles.botao, styles.botaoPress]
+                :
+                styles.botao}
+            >
+                <Text style={styles.txtBotao}>zerar</Text>
+            </Pressable>
+        </View>
+    );
+}
+
+export default atividade_3;
---
+++ components/atividade_3/styles.js
@@ -0,0 +1,43 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#d6eeff',
+        borderRadius: 20,
+        width:'100%',
+        alignItems: 'center',
+    },
+    titulo: {
+        fontSize: 30,
+        fontWeight: 'bold', 
+        marginVertical: 20,
+        color: '#673AB7'
+    },
+    botao: {
+        borderWidth: 3,
+        padding:10,
+        width: '80%',
+        borderRadius: 20,
+        alignItems: 'center',
+        borderColor: '#673AB7',
+    },
+    txtBotao: {
+        fontSize: 25,
+        color: '#673AB7',
+    },
+    botaoPress: {
+        backgroundColor: 'blue',
+    },
+    txtNumero: {
+        fontSize: 35,
+        color:'#673AB7',
+        marginVertical: 10,
+    }
+
+});
+
+export default styles;
+
+
+
